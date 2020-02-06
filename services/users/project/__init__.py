9aafb97473757918b5f18c92542f6cb72bffd158
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-06 22:13:34 +0200

    added postgres service

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,9 @@
+import datetime
 import os
+
 from flask import Flask, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -8,6 +12,22 @@ app = Flask(__name__)
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
 
+# db instance
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+# routes
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
