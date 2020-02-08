9902a166aa8c923a4d1830e2821b1c715372b2e6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-08 16:37:50 +0200

    added api module

---
+++ services/users/project/api/users.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, jsonify
+
+users_blueprint = Blueprint('users', __name__)
+
+@users_blueprint.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({'status': 'success','message': 'pong!'})
