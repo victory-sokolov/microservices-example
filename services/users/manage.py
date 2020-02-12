e05ba0e160bf302e689583dd215df833c6e84f5e
Viktor Sokolov <viktorsokolov.and@gmail.com>
2020-02-12 09:08:19 +0200

    added code coverage function

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,11 +1,17 @@
 import unittest
 
+import coverage
 from colour_runner import runner
 from flask import Flask
 from flask.cli import FlaskGroup
 
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True, include="project/*", omit=["project/tests/*", "project/config.py"]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -21,6 +27,7 @@ def seed_db():
 
 @cli.command("recreate_db")
 def recreate_db():
+    """Recreates Database"""
     db.drop_all()
     db.create_all()
     db.session.commit()
@@ -32,6 +39,22 @@ def test():
     tests = unittest.TestLoader().discover("project/tests", pattern="test*.py")
     result = runner.ColourTextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@cli.command("cov")
+def cov():
+    """Runs the unit tests with coverage"""
+    tests = unittest.TestLoader().discover("project/tests")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary:")
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     return 1
 
